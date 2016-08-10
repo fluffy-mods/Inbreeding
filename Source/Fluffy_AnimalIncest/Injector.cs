a5ed834b8b8efceffe0acfd471d0a5d031ba3395
FluffierThanThou <fluffy.l2032@gmail.com>
2016-08-10 14:36:48 +0200

    initial release

---
+++ Source/Fluffy_AnimalIncest/Injector.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CommunityCoreLibrary;
+using RimWorld;
+using Verse;
+
+namespace Fluffy_AnimalIncest
+{
+    public class Injector : SpecialInjector
+    {
+        public override bool Inject()
+        {
+            // set all animalIncest flags to false on all relationdefs.
+            foreach ( PawnRelationDef def in DefDatabase<PawnRelationDef>.AllDefsListForReading )
+                def.incestForAnimals = false;
+
+            return true;
+        }
+    }
+}
